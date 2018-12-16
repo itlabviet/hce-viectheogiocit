79f459f1a93b13d2c75da09866a47fedb4d693f5
hoaihuy <37462902+hoaihuy@users.noreply.github.com>
2018-12-16 10:47:16 +0700

    huy

--- wp-content/themes/html5blank-stable/footer.php
+++ wp-content/themes/html5blank-stable/footer.php
@@ -1,30 +1,32 @@
-			<!-- footer -->
-			<footer class="footer" role="contentinfo">
+	<footer class=" container-fluid">
 
-				<!-- copyright -->
-				<p class="copyright">
-					&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>. <?php _e('Powered by', 'html5blank'); ?>
-					<a href="//wordpress.org" title="WordPress">WordPress</a> &amp; <a href="//html5blank.com" title="HTML5 Blank">HTML5 Blank</a>.
-				</p>
-				<!-- /copyright -->
+	</footer>
+	
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src='<?php echo get_stylesheet_directory_uri() ?>/js/jquery.min.js'></script>
+<script src='<?php echo get_stylesheet_directory_uri() ?>/js/bootstrap.min.js'></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<script>
+	var aTop = $('#top_header').height();
+	$(document).ready(function(){
+	  $(window).scroll(function(){
 
-			</footer>
-			<!-- /footer -->
 
-		</div>
-		<!-- /wrapper -->
+	  	 if($(this).scrollTop()>=aTop)
+	  	 	{
+		    	$("#menu_fix nav").css("margin-top", "0px");
+			}
+			else
+			{
+				$("#menu_fix nav").css("margin-top", "30px");
+			}	
 
-		<?php wp_footer(); ?>
+	  });
+	});
+</script>
 
-		<!-- analytics -->
-		<script>
-		(function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
-		(f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
-		l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
-		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
-		ga('send', 'pageview');
-		</script>
-
-	</body>
+</script>
+</body>
 </html>
