3d26e65801ec86a32e4adf3a5bf978b32984ac36
hoaihuy <37462902+hoaihuy@users.noreply.github.com>
2018-12-16 10:21:34 +0700

    Update index.php

--- wp-content/themes/html5blank-stable/index.php
+++ wp-content/themes/html5blank-stable/index.php
@@ -134,11 +134,11 @@
 				      <img class="d-block w-100" src="<?php echo get_stylesheet_directory_uri() ?>/images/carousel/3.jpg" alt="Third slide">
 				    </div>
 				  </div>
-				  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+				  <a class="carousel-control-prev" style="z-index: 99;" href="#carouselExampleIndicators" role="button" data-slide="prev">
 				    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
 				    <span class="sr-only">Previous</span>
 				  </a>
-				  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+				  <a class="carousel-control-next" style="z-index:99;" href="#carouselExampleIndicators" role="button" data-slide="next">
 				    <span class="carousel-control-next-icon" aria-hidden="true"></span>
 				    <span class="sr-only">Next</span>
 				  </a>
@@ -223,8 +223,8 @@
 	</footer>
 	
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src='js/jquery.min.js'></script>
-<script src='js/bootstrap.min.js'></script>
+<script src='<?php echo get_stylesheet_directory_uri() ?>/js/jquery.min.js'></script>
+<script src='<?php echo get_stylesheet_directory_uri() ?>/js/bootstrap.min.js'></script>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
